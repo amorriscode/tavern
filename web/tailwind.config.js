7f604a599d36df17aacea8bc65c695e3f78aa4ed
Jet Simon <jetjdsimon@gmail.com>
2021-01-09 14:28:48 -0800

    add new logo/tagline + animation (#4)
    also add character sheet to navbar, does nothing still.

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -10,6 +10,15 @@ module.exports = {
         'brand-black': '#0A0C0C',
         'brand-white': '#F2F9FF',
       },
+      keyframes: {
+        blink: {
+          '0%, 100%': { opacity:1 },
+          '50%': { opacity:0 }
+        }
+      },
+      animation: {
+        blink: 'blink 1s ease-in-out infinite'
+      }
     },
   },
   variants: {
