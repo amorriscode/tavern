13a0a5b2d9e74ce08db4a20fb60c7238a548537c
Alex <me@a9e.sh>
2021-01-09 15:01:59 -0800

    Use difficulty name as ID and add difficulty seeding

--- api/db/seeds.js
+++ api/db/seeds.js
@@ -4,6 +4,32 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 const db = new PrismaClient()
+var updated = false;
+
+async function create_or_update_difficulty(name, color, description) {
+  const difficulty = await db.difficulty.findUnique({ where: { name } })
+  if (!difficulty) {
+    console.info(`Creating new difficulty: { ${name}, ${color}, ${description} }.`)
+    await db.difficulty.create({ data: { name, color, description } })
+    updated = true
+  } else {
+
+    if (difficulty.color !== color) {
+      console.info(`Updating ${name} color (${difficulty.color} -> ${color})..`)
+      updated = true
+    }
+
+    if (difficulty.description !== description) {
+      console.info(`Updating ${name} description..`)
+      updated = true
+    }
+
+    await db.difficulty.update({
+      where: { name },
+      data: { color, description }
+    })
+  }
+}
 
 async function main() {
   // Seed data is database data that needs to exist for your app to run.
@@ -16,7 +42,14 @@ async function main() {
   //     await db.user.create({ data: { name: 'Admin', email: 'admin@email.com' }})
   //   }
 
-  console.info('No data to seed. See api/db/seeds.js for info.')
+  // todo: use constants.js?
+  await create_or_update_difficulty('Easy', '#52fd74', 'An easy challenge.')
+  await create_or_update_difficulty('Medium', '#fdd852', 'A medium challenge.')
+  await create_or_update_difficulty('Hard', '#fd5251', 'A hard challenge.')
+
+  if (!updated) {
+    console.info('No data to seed.')
+  }
 }
 
 main()
