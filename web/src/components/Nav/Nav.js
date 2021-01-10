250c57a6f72721aae596823cd6e4af908c63dfbf
Alice Zhao <azhao991@gmail.com>
2021-01-09 20:14:53 -0800

    Authentication configured

--- web/src/components/Nav/Nav.js
+++ web/src/components/Nav/Nav.js
@@ -1,6 +1,7 @@
 import { Link, routes } from '@redwoodjs/router'
 
 import Logo from 'src/components/Logo'
+import AuthModal from 'src/components/AuthModal'
 
 const Nav = () => {
   return (
@@ -9,15 +10,19 @@ const Nav = () => {
         <Logo />
       </div>
 
-      <div className="w-1/2 space-x-4 text-right">
+      <div className="w-1/2 space-x-4 flex justify-end items-center">
         <Link to={routes.home()}>Guild</Link>
 
         <Link to={routes.home()}>Leaderboard</Link>
 
         <Link to={routes.home()}>Settings</Link>
+
+        <AuthModal />
       </div>
     </div>
   )
 }
 
 export default Nav
+
+//auth modal: has modal in it  modal
--- web/src/index.js
+++ web/src/index.js
@@ -1,3 +1,6 @@
+import { AuthProvider } from '@redwoodjs/auth'
+import { Magic } from 'magic-sdk'
+
 import ReactDOM from 'react-dom'
 import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
@@ -6,11 +9,17 @@ import Routes from 'src/Routes'
 
 import './index.css'
 
+
+
+const m = new Magic(process.env.MAGICLINK_PUBLIC)
+
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider>
-      <Routes />
-    </RedwoodProvider>
+    <AuthProvider client={m} type="magicLink">
+      <RedwoodProvider>
+        <Routes />
+      </RedwoodProvider>
+    </AuthProvider>
   </FatalErrorBoundary>,
   document.getElementById('redwood-app')
 )
