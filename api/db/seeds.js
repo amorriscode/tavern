be1021aa21d07265ee508c15f8d7ac5da35d127d
Alex <me@a9e.sh>
2021-01-09 17:04:02 -0800

    Add support for seeding problems off the filesystem

--- api/db/seeds.js
+++ api/db/seeds.js
@@ -1,12 +1,16 @@
 /* eslint-disable no-console */
 const { PrismaClient } = require('@prisma/client')
+const { promises: fs } = require('fs')
 const dotenv = require('dotenv')
+const problemRoot = '../src/problems'
+const extensionMap = { 'js': 'JAVASCRIPT' }
+const hash = require('object-hash');
 
 dotenv.config()
 const db = new PrismaClient()
 var updated = false;
 
-async function create_or_update_difficulty(name, color, description) {
+async function createOrUpdateDifficulty(name, color, description) {
   const difficulty = await db.difficulty.findUnique({ where: { name } })
   if (!difficulty) {
     console.info(`Creating new difficulty: { ${name}, ${color}, ${description} }.`)
@@ -31,6 +35,88 @@ async function create_or_update_difficulty(name, color, description) {
   }
 }
 
+
+async function seedProblems() {
+  const problems = await fs.readdir(problemRoot)
+  for (problem of problems) {
+    const stat = await fs.lstat(`${problemRoot}/${problem}`)
+    if (stat.isDirectory()) {
+      console.info(`Attempting to seed problem from directory '${problem}'..`)
+
+      let metadata
+      try {
+        metadata = require(`${problemRoot}/${problem}/metadata.json`)
+      } catch {
+        console.error(`Failed to fetch metadata.json for ${problem}!`)
+        continue
+      }
+
+      let rawTestCases
+      try {
+        rawTestCases = require(`${problemRoot}/${problem}/testCases.json`)
+      } catch {
+        console.error(`Failed to fetch testCases.json for ${problem}!`)
+        continue
+      }
+
+      const scaffolds = []
+      const scaffoldFiles = await fs.readdir(`${problemRoot}/${problem}/scaffolds`)
+      for (scaffoldName of scaffoldFiles) {
+        const extension = scaffoldName.split('.').pop()
+        const body = await fs.readFile(`${problemRoot}/${problem}/scaffolds/${scaffoldName}`, 'utf8')
+        const id = `${problem}-${extensionMap[extension].toLowerCase()}`
+
+        scaffolds.push({
+          where: { id },
+          create: {
+            id,
+            body,
+            language: extensionMap[extension]
+          }
+        })
+      }
+
+      const testCases = []
+      for (rawTestCase of rawTestCases) {
+        testCases.push({
+          input: JSON.stringify(rawTestCase.input),
+          output: JSON.stringify(rawTestCase.output)
+        })
+      }
+
+      const model = {
+        id: metadata.id,
+        name: metadata.name,
+        description: metadata.description,
+        difficulty: { connect: { name: metadata.difficulty } },
+        hints: metadata.hints,
+        sampleInput: metadata.sample.input,
+        sampleOutput: metadata.sample.output,
+        testCases: { create: testCases },
+        scaffolds: { connectOrCreate: scaffolds }
+      }
+
+      model['checksum'] = hash(model)
+
+      const existing = await db.problem.findUnique({ where: { id: metadata.id } })
+      if (existing) {
+        if (model.checksum !== existing.checksum) {
+          console.info(`Outdated checksum for problem ${model.id} (${problem})! Updating DB.`)
+          await db.problem.update({
+            where: { id: metadata.id },
+            data: model
+           })
+          updated = true
+        }
+      } else {
+        console.info(`Created new problem in database ${model.id} (${problem})!`)
+        await db.problem.create({ data: foreignKeyReplacement(model) })
+        updated = true
+      }
+    }
+  }
+}
+
 async function main() {
   // Seed data is database data that needs to exist for your app to run.
   // Ideally this file should be idempotent: running it multiple times
@@ -43,9 +129,11 @@ async function main() {
   //   }
 
   // todo: use constants.js?
-  await create_or_update_difficulty('Easy', '#52fd74', 'An easy challenge.')
-  await create_or_update_difficulty('Medium', '#fdd852', 'A medium challenge.')
-  await create_or_update_difficulty('Hard', '#fd5251', 'A hard challenge.')
+  await createOrUpdateDifficulty('Easy', '#52fd74', 'An easy challenge.')
+  await createOrUpdateDifficulty('Medium', '#fdd852', 'A medium challenge.')
+  await createOrUpdateDifficulty('Hard', '#fd5251', 'A hard challenge.')
+
+  await seedProblems()
 
   if (!updated) {
     console.info('No data to seed.')
@@ -57,3 +145,17 @@ main()
   .finally(async () => {
     await db.$disconnect()
   })
+
+const foreignKeyReplacement = (input) => {
+  let output = input
+  const foreignKeys = Object.keys(input).filter((k) => k.match(/Id$/))
+  foreignKeys.forEach((key) => {
+    const modelName = key.replace(/Id$/, '')
+    const value = input[key]
+    delete output[key]
+    output = Object.assign(output, {
+      [modelName]: { connect: { id: value } },
+    })
+  })
+  return output
+}
